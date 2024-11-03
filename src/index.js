473651660a00804470570186e3f1de20257292d4
awoutz <163394646+awoutz@users.noreply.github.com>
2024-11-03 22:08:25 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Si tu utilises un fichier CSS
+import KarenDashboard from './KarenDashboard';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <KarenDashboard />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
